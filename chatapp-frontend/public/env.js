b1bfb4a5aa6e0ddeea5c682950f4fef6581f0670
ybaghiyev <ybaghiyev@IT_DEP_10>
2026-02-24 14:55:16 +0400

    reversing-proxy

--- chatapp-frontend/public/env.js
+++ chatapp-frontend/public/env.js
@@ -1,5 +1,5 @@
 // Bu fayl Docker container başlayanda docker-entrypoint.sh tərəfindən yenidən yazılır.
 // Development-də (npm run dev) bu default dəyərlər istifadə olunur.
 window.__ENV__ = {
-  API_BASE_URL: "",
+  API_BASE_URL: "http://localhost:7000",
 };
